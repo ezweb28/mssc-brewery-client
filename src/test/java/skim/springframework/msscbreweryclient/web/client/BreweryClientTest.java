f60167e7affe4c941679138a6dff45121dc1b14b
Sunpil Kim <ezweb28@gmail.com>
2022-12-03 01:26:38 -0500

    HTTP POST with Spring RestTemplate

--- src/test/java/skim/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/skim/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import skim.springframework.msscbreweryclient.web.model.BeerDto;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,5 +21,17 @@ class BreweryClientTest {
         BeerDto dto = client.getBeerById(UUID.randomUUID());
 
         assertNotNull(dto);
+    }
+
+    @Test
+    void testSaveNewBeer() {
+        //given
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+
+        URI uri = client.saveNewBeer(beerDto);
+
+        assertNotNull(uri);
+
+        System.out.println(uri.toString());
     }
 }
