5f2a81709fe903d45f080ffe09699772f9807b2f
Sunpil Kim <ezweb28@gmail.com>
2022-12-02 22:09:38 -0500

    HTTP GET with Spring RestTemplate

---
+++ src/test/java/skim/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,24 @@
+package skim.springframework.msscbreweryclient.web.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import skim.springframework.msscbreweryclient.web.model.BeerDto;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto = client.getBeerById(UUID.randomUUID());
+
+        assertNotNull(dto);
+    }
+}
