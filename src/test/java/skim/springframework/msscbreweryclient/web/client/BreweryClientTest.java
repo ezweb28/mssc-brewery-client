fa33d6e900e08c45d4bc31a42538dce25dee7b48
Sunpil Kim <ezweb28@gmail.com>
2022-12-03 21:29:43 -0500

    Added HTTP POST, PUT, DELETE for Customer

--- src/test/java/skim/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/skim/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import skim.springframework.msscbreweryclient.web.model.BeerDto;
+import skim.springframework.msscbreweryclient.web.model.CustomerDto;
 
 import java.net.URI;
 import java.util.UUID;
@@ -48,5 +49,38 @@ class BreweryClientTest {
     void testDeleteBeer() {
 
         client.deleteBeer(UUID.randomUUID());
+    }
+
+    @Test
+    void getCustomerById() {
+        CustomerDto dto = client.getCustomerById(UUID.randomUUID());
+
+        assertNotNull(dto);
+    }
+
+    @Test
+    void testSaveNewCustomer() {
+        //given
+        CustomerDto customerDto = CustomerDto.builder().name("Joe").build();
+
+        URI uri = client.saveNewCustomer(customerDto);
+
+        assertNotNull(uri);
+
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void testUpdateCustomer() {
+        //given
+        CustomerDto customerDto = CustomerDto.builder().name("Jim").build();
+
+        client.updateCustomer(UUID.randomUUID(), customerDto);
+
+    }
+
+    @Test
+    void testDeleteCustomer() {
+        client.deleteCustomer(UUID.randomUUID());
     }
 }
